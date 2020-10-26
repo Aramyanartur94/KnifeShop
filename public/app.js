5a86bebd78d561745dd36c2d05b6bb3468b9213e
Aramyanartur94 <anastasiasemilova2012@yandex.ru>
2020-10-26 11:16:07 +0800

    Update orders input field

--- public/app.js
+++ public/app.js
@@ -6,7 +6,15 @@ function add_to_cart(id)
 		window.localStorage.setItem(key, x)
 
 		alert('Items in your cart: ' + cart_get_number_of_items());
+
+		update_orders_input()
 	}
+
+function update_orders_input()
+{
+	var orders = cart_get_orders();
+	$('#orders_input').val(orders);
+}
 
 function cart_get_number_of_items()
 	{
@@ -22,4 +30,20 @@ function cart_get_number_of_items()
 			}
 		}
 		return cnt;
+	}	
+
+function cart_get_orders()
+	{
+		var orders = '';
+		for(var i = 0; i < window.localStorage.length; i++)
+		{
+			var key = window.localStorage.key(i); // Получаем ключ;
+			var value = window.localStorage.getItem(key); //Получаем значение; Аналог в руби: hh['bbb'] = x
+			
+			if(key.indexOf('Product_') == 0)
+			{
+				orders = orders + key + '=' + value + ',';
+			}
+		}
+		return orders;
 	}	
